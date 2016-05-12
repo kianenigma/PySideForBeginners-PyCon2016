a2bce7bd8a856a1ea4e29a2b6e59282577c0a983
Kianenigma <kian.peymani@gmail.com>
2016-05-12 19:22:27 +0430

    3- Classic Stuff

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 from PySide import QtGui
 
-class Example(QtGui.QWidget):
+class Example(QtGui.QMainWindow):
 
     def __init__(self):
         # Call the parent constructor
@@ -10,17 +10,46 @@ class Example(QtGui.QWidget):
         self.init_ui()
 
     def init_ui(self):
+        # now we have a Status bar!
+        self.statusBar().showMessage('Application is ready!')
+
+        # add a Menu Bar and Toolbar
+        # We must first create an action
+        exit_action = QtGui.QAction(QtGui.QIcon('pyside.png'), 'Exit', self)
+        exit_action.setShortcut('Ctrl+Q')
+        exit_action.setStatusTip('Exit application')
+        # more on .connect later
+        exit_action.triggered.connect(self.close)
+
+        dummy_action = QtGui.QAction('Dummy' , self)
+        dummy_action.setStatusTip('I Will do nothing')
+
+        # Create a Menu Bar
+        menu_bar = self.menuBar()
+        file_menu = menu_bar.addMenu('File')
+        file_menu.addAction(exit_action)
+        file_menu.addAction(dummy_action)
+
+        # Create o Toolbar
+        toolbar = self.addToolBar('Exit')
+        toolbar.addAction(exit_action)
+        toolbar.addAction(dummy_action)
+
         # set size, title, icon
         self.setGeometry(300, 300, 300, 300)
         self.setWindowTitle('Hello PySide')
-        self.setWindowIcon(QtGui.QIcon('./resources/logo.png'))
+
+        # set a tooltip for the parent widget
+        self.setToolTip('This is a <b>QWidget</b> widget')
 
         # add more widgets
-        self.page_title = QtGui.QLabel("Hello again", self)
-        self.page_title.move(100, 5)
+        page_title = QtGui.QLabel("Hello again", self)
+        page_title.move(100, 80)
+        page_title.setToolTip('This is a <b>Label</b> widget')
 
-        self.btn = QtGui.QPushButton("Click me", self)
-        self.btn.move(100, 30)
+        btn = QtGui.QPushButton("Click me", self)
+        btn.move(100, 100)
+        btn.setToolTip('This is a <b>QPushButton</b> widget')
 
         self.show()
 
